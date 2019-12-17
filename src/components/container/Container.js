d20c473f7cb3763f97d26ddd20dad869c88b068b
Davit Burnazyan <davitburnazyan>
2019-12-17 16:59:34 +0400

    update Container

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
+import React from 'react';
 import Map from '../styled';
 
-class Container extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            clicked: null
-        }
+const Container = (props) => {
+    const handleClick = (id,image) => {
+        props.chooseObject(id,image)
     }
-    handleClick = (id,image) => {
-        this.props.chooseObject(id,image)
+    const renderView = () => {
+        return props.initialState.map((event) => {
+            const { id, image } = event;
+            if(event.status)
+                return <Map key={id} status={true} image={image} onClick={() => {handleClick(id,image)}} />
+            else
+                return <Map key={id} status={false} image={image} onClick={() => {handleClick(id, image)}} />
+        })
     }
-    render() {
-        const renderView = () => {
-            return this.props.initialState.map((event) => {
-                const { id, image } = event;
-                if(event.status)
-                    return <Map key={id} status={true} image={image} onClick={() => {this.handleClick(id,image)}}/>
-                else
-                    return <Map key={id} status={false} image={image} onClick={() => {this.handleClick(id, image)}}/>
-            })
-        }
-        return (
-            <>
-                {renderView()}
-            </>
-        )
-    }
+    return (
+        <>
+            {renderView()}
+        </>
+    )
+
 }
 
 export default Container;
